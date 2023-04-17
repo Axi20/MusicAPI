ac11a1a068208b7be92c079b87a6595585a219d4
Varró Ágnes <90404630+Axi20@users.noreply.github.com>
2023-04-17 09:58:15 +0200

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,1 +1,12 @@
-
+function sendPost(){
+    const data = JSON.stringify({
+        title: document.getElementById("title").value,
+        singer: document.getElementById("singer").value,
+        genre:document.getElementById("genre").value
+        timeline:document.getElementById("timeline").value
+      
+      });
+      
+      navigator.sendBeacon('http://127.0.0.1:5000/savedetails/', data);
+      console.log(data);
+    }
