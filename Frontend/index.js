55d870eaf1480b7278c2bc04b655f5e77d4f87cb
Varró Ágnes <90404630+Axi20@users.noreply.github.com>
2023-04-17 12:39:23 +0200

    Update index.js

--- Frontend/index.js
+++ Frontend/index.js
@@ -3,27 +3,23 @@ var id = "view";
 
 async function generator(url, id) {
     var request = await new XMLHttpRequest()
-
-request.open('GET', url, true)
-request.onload = function () {
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response)
-view(data, request, id);
-
+    request.open('GET', url, true)
+    request.onload = function () {
+      var data = JSON.parse(this.response)
+      view(data, request, id);
+    }
+  request.send()
 }
-
-request.send()
-  }
 
   function view(data, request, id){
       if(id == "view"){
-    if (request.status >= 200 && request.status < 400) {
-         data.forEach((query) => {
-          console.log(request.status);
-          var div = document.createElement("tr");
-            var mainContainer = document.getElementById(id);
-          div.innerHTML = "<td>"+query.id+"</td><td><input id='title"+query.id+"' placeholder='"+query.title+"' value='"+query.title+"'/></td><td><input id='singer"+query.id+"' placeholder='"+query.singer+"' value='"+query.singer+"'/></td><td><input id='genre"+query.id+"' placeholder='"+query.genre+"' value='"+query.genre+"'/></td><td><input id='timeline'+query.id+"' placeholder='"query.timeline+"' value='"+query.timeline+"'/></td>"+"<button onclick = 'deleterecord("+query.id+")' type = 'submit' value='Submit'>Delete</button>"+"<button onclick = 'update("+query.id+")'>Update</button>" ;
-          mainContainer.appendChild(div)
+        if (request.status >= 200 && request.status < 400) {
+           data.forEach((query) => {
+           console.log(request.status);
+           var div = document.createElement("tr");
+           var mainContainer = document.getElementById(id);
+           div.innerHTML = "<th>"+query.id+"</th><th><input id='title"+query.id+"' value='"+query.title+"'/></th><th><input id='singer"+query.id+"' value='"+query.singer+"'/></th><th><input id='genre"+query.id+"' value='"+query.genre+"'/></th><th><input id='timeline"+query.id+"' value='"+query.timeline+"'/></th>"+"<button onclick = 'deleterecord("+query.id+")' type = 'submit' value='Submit'>Delete</button>"+"<button onclick = 'update("+query.id+")'>Update</button>" ;
+           mainContainer.appendChild(div)
         })
       } else {
         console.log('error')
@@ -31,7 +27,7 @@ request.send()
   }
 
 async function generate_html(){
-await generator(url, id);
+  await generator(url, id);
 }
 
 function deleterecord(id){
@@ -47,9 +43,8 @@ function update(id){
     id: id,
     title: document.getElementById("title"+id).value,
     singer: document.getElementById("singer"+id).value,
-    genre:document.getElementById("genre"+id).value
+    genre:document.getElementById("genre"+id).value,
     timeline:document.getElementById("timeline"+id).value
-    
   });
   
   navigator.sendBeacon('http://127.0.0.1:5000/updatedetails/', data);
